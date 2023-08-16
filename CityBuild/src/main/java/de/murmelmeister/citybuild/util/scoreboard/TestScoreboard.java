aea82c00a5f9a2e56355b7af1d1641500ea27ebc
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2023-08-16 12:32:57 +0200

    Scoreboard fix (CityBuild)

--- CityBuild/src/main/java/de/murmelmeister/citybuild/util/scoreboard/TestScoreboard.java
+++ CityBuild/src/main/java/de/murmelmeister/citybuild/util/scoreboard/TestScoreboard.java
@@ -9,7 +9,9 @@ import de.murmelmeister.citybuild.configs.Message;
 import de.murmelmeister.citybuild.util.HexColor;
 import de.murmelmeister.citybuild.util.config.Configs;
 import de.murmelmeister.citybuild.util.config.Messages;
-import de.murmelmeister.murmelapi.permission.User;
+import de.murmelmeister.murmelapi.permission.Group;
+import de.murmelmeister.murmelapi.permission.Permission;
+import de.murmelmeister.murmelapi.permission.PermissionConfig;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -36,7 +38,7 @@ public class TestScoreboard extends ScoreboardBuilder {
     @Override
     public void update() {
         schedulerTask.addBukkitTask(player,
-                server.getScheduler().runTaskTimerAsynchronously(instance, () -> setScoreboard(), 2L, config.getLong(Configs.SCOREBOARD_UPDATE_SCORE_TIMER) * 20L));
+                server.getScheduler().runTaskTimerAsynchronously(instance, this::setScoreboard, 2L, config.getLong(Configs.SCOREBOARD_UPDATE_SCORE_TIMER) * 20L));
     }
 
     private void setScoreboard() {
@@ -44,77 +46,79 @@ public class TestScoreboard extends ScoreboardBuilder {
         this.server = main.getInstance().getServer();
         this.config = main.getConfig();
         Message message = main.getMessage();
-        User user = main.getPermission().getUser();
+        Permission permission = main.getPermission();
+        Group group = permission.getGroup();
         Economy economy = main.getEconomy();
         DecimalFormat decimalFormat = new DecimalFormat(config.getString(Configs.PATTERN_DECIMAL));
         setDisplayName(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_DISPLAY_NAME)));
-        /*ranks.getRankList().forEach(s -> {
-            if (player.hasPermission(ranks.getPermission(s))) {
+
+        group.groups().forEach(s -> {
+            if (player.hasPermission(permission.getValueString(PermissionConfig.DEFAULT_PRE_PERMISSION) + s)) {
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_15))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_15)
-                            .replace("[RANK_COLOR]", user.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 15);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_14))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_14)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 14);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_13))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_13)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 13);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_12))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_12)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 12);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_11))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_11)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 11);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_10))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_10)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 10);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_9))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_9)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 9);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_8))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_8)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 8);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_7))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_7)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 7);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_6))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_6)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 6);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_5))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_5)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 5);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_4))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_4)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 4);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_3))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_3)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 3);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_2))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_2)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 2);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_1))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_1)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 1);
                 if (config.getBoolean(Configs.SCOREBOARD_ENABLE_SCORE_0))
                     setScoreTeam(HexColor.format(message.getString(Messages.SCOREBOARD_SCORE_0)
-                            .replace("[RANK_COLOR]", ranks.getTabColor(s)).replace("[RANK]", ranks.getScoreboard(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
+                            .replace("[RANK_COLOR]", group.scoreboardColor(s)).replace("[RANK]", group.scoreboardPrefix(s)).replace("[SERVER]", config.getString(Configs.CURRENT_SERVER))
                             .replace("[CURRENCY]", config.getString(Configs.ECONOMY_CURRENCY)).replace("[MONEY]", decimalFormat.format(economy.getMoney(player.getUniqueId())))), 0);
             }
-        });*/
+        });
     }
 }
