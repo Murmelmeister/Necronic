015aadd6ac785cb27523485c911f1e18b5fb63d9
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-06 22:05:58 +0200

    Refactor CustomItem interface and implement CustomItemProvider for enhanced item management

--- CityBuild/src/main/java/de/murmelmeister/citybuild/api/item/CustomItem.java
+++ CityBuild/src/main/java/de/murmelmeister/citybuild/api/item/CustomItem.java
@@ -1,25 +1,162 @@
 package de.murmelmeister.citybuild.api.item;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
 import java.util.UUID;
 
-public sealed interface CustomItem permits CustomItemImpl {
-    UUID getId();
+public sealed interface CustomItem permits CustomItemProvider {
+    /**
+     * Checks whether an item with the specified unique identifier exists.
+     *
+     * @param id The unique identifier of the item to check
+     * @return True if the item exists, false otherwise
+     */
+    boolean existsItem(UUID id);
 
-    String getDisplayName();
+    /**
+     * Adds a custom item with the specified display name, icon name, and material.
+     *
+     * @param displayName The display name of the custom item
+     * @param iconName    The icon name of the custom item
+     * @param material    The material representing the custom item
+     */
+    void addItem(String displayName, String iconName, Material material);
 
-    void setDisplayName(String displayName);
+    /**
+     * Adds a new custom item with the specified display name, icon name, material, and description.
+     *
+     * @param displayName The display name of the item. It represents the visual name the user sees.
+     * @param iconName    The icon name of the item. It specifies the visual representation or text component for the icon.
+     * @param material    The material type of the item. It determines the type of the item in terms of Minecraft material.
+     * @param description The description of the item. It provides additional details or lore about the item.
+     */
+    void addItem(String displayName, String iconName, Material material, String description);
 
-    Material getMaterial();
+    /**
+     * Adds a custom item with the specified ID, display name, icon name, and material.
+     *
+     * @param id          The unique identifier for the custom item.
+     * @param displayName The display name of the custom item.
+     * @param iconName    The icon name representing the custom item visually.
+     * @param material    The material type of the custom item.
+     */
+    void addItem(UUID id, String displayName, String iconName, Material material);
 
-    void setMaterial(Material material);
+    /**
+     * Adds a new custom item with the specified properties to the system.
+     *
+     * @param id          The unique identifier for the custom item.
+     * @param displayName The display name of the custom item.
+     * @param iconName    The icon name representing the custom item.
+     * @param material    The {@link Material} type of the custom item.
+     * @param description Optional description of the custom item.
+     */
+    void addItem(UUID id, String displayName, String iconName, Material material, String description);
 
-    String getDescription();
+    /**
+     * Removes a custom item identified by the given unique identifier.
+     *
+     * @param id The UUID of the custom item to be removed
+     */
+    void removeItem(UUID id);
 
-    void setDescription(String description);
+    /**
+     * Updates an existing custom item in the database with the specified parameters.
+     *
+     * @param id          The unique identifier of the custom item to be updated.
+     * @param displayName The new display name of the custom item.
+     * @param iconName    The new icon name of the custom item, representing its visual name.
+     * @param material    The new material type of the custom item.
+     * @param description The new description of the custom item, providing additional details.
+     */
+    void updateItem(UUID id, String displayName, String iconName, Material material, String description);
 
-    String[] getLore();
+    /**
+     * Retrieves a list of unique identifiers for all custom items.
+     *
+     * @return A list of UUIDs representing the IDs of all custom items.
+     */
+    List<UUID> getCustomItemIds();
 
-    void setLore(String[] lore);
+    /**
+     * Retrieves a list of internal names for all custom items.
+     *
+     * @return A list of strings representing the internal names of all custom items.
+     */
+    List<String> getInternNames();
+
+    /**
+     * Retrieves the unique identifier of a custom item based on its internal name.
+     *
+     * @param internName The internal name of the custom item
+     * @return The UUID of the custom item, or null if no item exists with the given internal name
+     */
+    UUID getCustomItemId(String internName);
+
+    /**
+     * Retrieves the internal name of a custom item associated with the given UUID.
+     *
+     * @param id The unique identifier of the custom item
+     * @return The internal name of the custom item, or null if no item is found with the given UUID
+     */
+    String getInternName(UUID id);
+
+    /**
+     * Retrieves the display name of a custom item associated with the given UUID.
+     *
+     * @param id The unique identifier of the custom item
+     * @return The display name of the custom item, or null if no item is found with the given UUID
+     */
+    Component getDisplayName(UUID id);
+
+    /**
+     * Retrieves the Material associated with the custom item identified by the given UUID.
+     *
+     * @param id The unique identifier of the custom item
+     * @return The Material corresponding to the specified item, or null if no item is found with the given UUID
+     */
+    Material getMaterial(UUID id);
+
+    /**
+     * Retrieves the description associated with a specific custom item identified by its UUID.
+     *
+     * @param id The unique identifier of the custom item
+     * @return The description of the custom item, or null if no description is found
+     */
+    String getDescription(UUID id);
+
+    /**
+     * Retrieves the lore of a custom item associated with the given UUID.
+     * The lore is derived from the item's description, with each line
+     * of the description representing an individual element in the returned array.
+     *
+     * @param id The unique identifier of the custom item
+     * @return An array of strings representing the lore of the item,
+     * or null if no description is found for the specified item
+     */
+    String[] getLore(UUID id);
+
+    /**
+     * Constructs and returns the {@link ItemStack} associated with the given UUID.
+     * The ItemStack's metadata such as display name, icon, material, and lore
+     * is determined and set based on the data associated with the UUID.
+     *
+     * @param id The unique identifier of the custom item
+     * @return The {@link ItemStack} corresponding to the unique identifier
+     */
+    ItemStack getItemStack(UUID id);
+
+    /**
+     * Adds default materials to the custom items collection. This method iterates over all available
+     * materials in the application, except those categorized as "air" or non-item materials, and adds
+     * them as custom items if they do not already exist.
+     * <p>
+     * Each eligible material is assigned a new unique identifier (UUID) and is stored with its display
+     * name and icon name derived from the material's internationalized display name. If a material
+     * already exists in the collection, it is skipped.
+     */
+    void addDefaultMaterials();
 }
--- CityBuild/src/main/java/de/murmelmeister/citybuild/api/item/CustomItemProvider.java
+++ CityBuild/src/main/java/de/murmelmeister/citybuild/api/item/CustomItemProvider.java
@@ -1,20 +1,178 @@
 package de.murmelmeister.citybuild.api.item;
 
+import de.murmelmeister.citybuild.CityBuild;
+import de.murmelmeister.murmelapi.database.Database;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
-public sealed interface CustomItemProvider permits CustomItemProviderImpl {
-    CustomItem getCustomItem(UUID id);
+public final class CustomItemProvider implements CustomItem {
+    private static final String TABLE_NAME = "CB_CustomItems";
+    private final Database database;
 
-    boolean containsCustomItem(UUID id);
+    public CustomItemProvider(Database database) {
+        this.database = database;
+        createTable();
+        Procedure.loadAll(database);
+    }
 
-    void addCustomItem(CustomItem customItem);
+    private void createTable() {
+        database.createTable(TABLE_NAME, "ID UUID PRIMARY KEY, InternName TINYTEXT UNIQUE, DisplayName TINYTEXT, Material TINYTEXT, Description MEDIUMTEXT");
+    }
 
-    void removeCustomItem(UUID id);
+    @Override
+    public boolean existsItem(UUID id) {
+        return id != null && database.existsCallable(Procedure.GET_DATA.getName(), id.toString());
+    }
 
-    void updateCustomItem(CustomItem customItem);
+    @Override
+    public void addItem(String internName, String displayName, Material material) {
+        addItem(internName, displayName, material, null);
+    }
 
-    List<UUID> getCustomItemIds();
+    @Override
+    public void addItem(String internName, String displayName, Material material, String description) {
+        addItem(UUID.randomUUID(), internName, displayName, material, description);
+    }
 
-    void addDefaultMaterials();
+    @Override
+    public void addItem(UUID id, String internName, String displayName, Material material) {
+        addItem(id, internName, displayName, material, null);
+    }
+
+    @Override
+    public void addItem(UUID id, String internName, String displayName, Material material, String description) {
+        database.updateCallable(Procedure.ADD_ITEM.getName(), id.toString(), internName, displayName, material.name(), description);
+    }
+
+    @Override
+    public void removeItem(UUID id) {
+        database.updateCallable(Procedure.REMOVE_ITEM.getName(), id.toString());
+    }
+
+    @Override
+    public void updateItem(UUID id, String displayName, String iconName, Material material, String description) {
+        database.updateCallable(Procedure.UPDATE_ITEM.getName(), id.toString(), displayName, iconName, material.name(), description);
+    }
+
+    @Override
+    public List<UUID> getCustomItemIds() {
+        return database.queryListCallable(Procedure.GET_ALL.getName(), new LinkedList<>(), resultSet -> UUID.fromString(resultSet.getString("ID")));
+    }
+
+    @Override
+    public List<String> getInternNames() {
+        return database.queryListCallable(Procedure.GET_ALL.getName(), new LinkedList<>(), resultSet -> resultSet.getString("InternName"));
+    }
+
+    @Override
+    public UUID getCustomItemId(String internName) {
+        return database.queryCallable(Procedure.GET_ID_BY_NAME.getName(), null, resultSet -> {
+            String id = resultSet.getString("ID");
+            return id != null ? UUID.fromString(id) : null;
+        }, internName);
+    }
+
+    @Override
+    public String getInternName(UUID id) {
+        return database.queryCallable(Procedure.GET_DATA.getName(), null, resultSet -> resultSet.getString("InternName"), id.toString());
+    }
+
+    @Override
+    public Component getDisplayName(UUID id) {
+        return database.queryCallable(Procedure.GET_DATA.getName(), null,
+                resultSet -> MiniMessage.miniMessage().deserialize(resultSet.getString("DisplayName")), id.toString());
+    }
+
+    @Override
+    public Material getMaterial(UUID id) {
+        return database.queryCallable(Procedure.GET_DATA.getName(), null, resultSet -> {
+            String material = resultSet.getString("Material");
+            return material != null ? Material.getMaterial(material) : null;
+        }, id.toString());
+    }
+
+    @Override
+    public String getDescription(UUID id) {
+        return database.queryCallable(Procedure.GET_DATA.getName(), null, resultSet -> resultSet.getString("Description"), id.toString());
+    }
+
+    @Override
+    public String[] getLore(UUID id) {
+        String description = getDescription(id);
+        return description != null ? description.split("\n") : null;
+    }
+
+    @Override
+    public ItemStack getItemStack(UUID id) {
+        Material material = getMaterial(id);
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+        // container.set(CityBuild.getKeyCustomItems(), PersistentDataType.STRING, id.toString());
+        container.set(CityBuild.getKeyCustomItems(), PersistentDataType.BOOLEAN, true);
+        itemMeta.displayName(getDisplayName(id));
+
+        List<Component> lores = new LinkedList<>();
+        String[] lore = getLore(id);
+        if (lore != null)
+            for (String line : lore)
+                lores.add(MiniMessage.miniMessage().deserialize(line));
+        itemMeta.lore(lores);
+
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+    @Override
+    public void addDefaultMaterials() {
+        for (Material material : Material.values()) {
+            if (material.isAir()) continue;
+            if (!material.isItem()) continue;
+            UUID id = UUID.randomUUID();
+            if (existsItem(id)) continue;
+            String displayName = new ItemStack(material).getI18NDisplayName();
+            addItem(id, material.name().toLowerCase(), displayName, material);
+        }
+    }
+
+    private enum Procedure {
+        ADD_ITEM("CB_CustomItems_AddItem", "cid UUID, iname TINYTEXT, display TINYTEXT, mat TINYTEXT, des MEDIUMTEXT",
+                "INSERT INTO [TABLE] VALUES (cid,iname,display,mat,des);"),
+        REMOVE_ITEM("CB_CustomItems_RemoveItem", "cid UUID", "DELETE FROM [TABLE] WHERE ID=cid;"),
+        GET_DATA("CB_CustomItems_GetData", "cid UUID", "SELECT * FROM [TABLE] WHERE ID=cid;"),
+        GET_ID_BY_NAME("CB_CustomItems_GetIdByName", "iname TINYTEXT", "SELECT ID FROM [TABLE] WHERE InternName=iname;"),
+        GET_ALL("CB_CustomItems_GetAll", "", "SELECT ID, InternName FROM [TABLE];"),
+        UPDATE_ITEM("CB_CustomItems_UpdateItem", "cid UUID, display TINYTEXT, iname TINYTEXT, mat TINYTEXT, des MEDIUMTEXT",
+                "UPDATE [TABLE] SET DisplayName=display, IconName=iname, Material=mat, Description=des WHERE ID=cid;");
+        private static final Procedure[] VALUES = values();
+
+        private final String name;
+        private final String query;
+
+        Procedure(final String name, final String input, final String query) {
+            this.name = name;
+            this.query = Database.getProcedureQuery(name, input, query);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getQuery() {
+            return query.replace("[TABLE]", TABLE_NAME);
+        }
+
+        public static void loadAll(Database database) {
+            for (Procedure procedure : VALUES) database.update(procedure.getQuery());
+        }
+    }
 }
